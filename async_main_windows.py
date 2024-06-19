a3d04c82232194bcf1243e5282fb2c27ee665d16
RamenRa <53000259+RamenRa@users.noreply.github.com>
2024-06-19 20:37:01 +0800

    Add files via upload

---
+++ async_main_windows.py
@@ -0,0 +1,254 @@
+import sys
+import asyncio
+import os
+import re
+from typing import List, Tuple
+import time
+ffmpeg_flag = False
+
+def check_media(path: str) -> list:
+    # folders = []
+    # for root, dirs, files in os.walk(path):
+    #     for dir_name in dirs:
+    #         # 构造完整的文件夹路径
+    #         full_path = os.path.join(root, dir_name)
+    #         folders.append(full_path)
+    folders = []
+    for folder in os.listdir(path):
+        full_path = os.path.join(path, folder)
+        if os.path.isdir(full_path):
+            folders.append(full_path)
+    return folders
+
+
+def tag_folder(folders: List[str], ext: str) -> List[str]:
+    matching_folders = []
+    for folder in folders:
+        if ext:
+            # 如果扩展名不为空，构造正则表达式匹配纯数字文件名并且有指定扩展名
+            pattern = re.compile(r'^\d+\.' + re.escape(ext) + r'$')
+        else:
+            # 如果扩展名为空，只匹配纯数字文件名
+            pattern = re.compile(r'^\d+$')
+
+        numeric_files = [file for file in os.listdir(folder) if pattern.match(file)]
+        if len(numeric_files) >= 5:  # 确保文件数大于等于5
+            matching_folders.append(folder)
+    return matching_folders
+
+def classify_folders(folders: List[str], key_ext: str) -> Tuple[List[str], List[str], List[str], List[str]]:
+    encrypted_folders = []
+    unencrypted_folders = []
+    key_files = []
+    key_map = []
+
+    for folder in folders:
+        if os.path.exists(folder + '.m3u8'):  # 删除之前生成的.m3u8
+            os.remove(folder + '.m3u8')
+        if os.path.exists(folder + '.txt'):
+            os.remove(folder + '.txt')  # 删除之前生成的.txt
+
+        has_key_file = False
+        current_key_files = []
+        current_auxiliary_files = []
+        has_added_aux_file = False  # 每个文件夹只允许一个m3u8文件
+
+        for file in os.listdir(folder):
+            file_path = os.path.join(folder, file)
+            if file.endswith('.' + key_ext):
+                has_key_file = True
+                current_key_files.append(file_path)
+            if (len(file) > 10 or file.endswith('.m3u8')) and not has_added_aux_file:
+                current_auxiliary_files.append(file_path)
+                has_added_aux_file = True  # 每个文件夹只允许一个m3u8文件
+
+        if has_key_file:
+            encrypted_folders.append(folder)
+            key_files.extend(current_key_files)
+            key_map.extend(current_auxiliary_files)
+        else:
+            unencrypted_folders.append(folder)
+    return encrypted_folders, unencrypted_folders, key_files, key_map
+
+
+def ffmpeg_cmd(new_m3u8: str, output_file: str, function: str) -> None:
+    if ffmpeg_flag:   # 好像会更快
+        if function == "normal":
+            (
+                ffmpeg
+                .input(new_m3u8, f='concat', safe=0)
+                .output(output_file, c='copy')
+                .run(capture_stderr=True)
+            )
+        elif function == "aes128":
+            (
+                ffmpeg
+                .input(new_m3u8, allowed_extensions='ALL')
+                .output(output_file, c='copy')
+                .run(capture_stderr=True)
+            )
+    else:
+        ffmpeg_path = os.path.join(os.path.dirname(sys.argv[0]), '_internal/ffmpeg/ffmpeg.exe').replace(os.sep, '/')
+        if function == "normal":
+            cmd = f'{ffmpeg_path} -f concat -safe 0 -i "{new_m3u8}" -c copy "{output_file}"'
+            os.system(cmd)
+        elif function == "aes128":
+            cmd = f'{ffmpeg_path} -allowed_extensions ALL -i "{new_m3u8}" -c copy "{output_file}"'
+            os.system(cmd)
+
+async def cmd_pool(new_m3u8: str, output_file: str, function: str, semaphore: asyncio.Semaphore) -> None:
+    output_file = output_file.replace('.m3u8_contents', '')
+    async with semaphore:  # 使用信号量限制并发数量
+        try:
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None, ffmpeg_cmd, new_m3u8, output_file, function)
+        except ffmpeg.Error as e:
+            print('ffmpeg error:', e.stderr.decode('utf8'))
+            raise
+
+async def AES128(encrypted_folders: List[str], key_files: List[str], key_map: List[str], ts_end: str, semaphore: asyncio.Semaphore):
+    async def sort_func(path: str) -> List[int]:
+        file_list = []
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                if len(filename) < 10 and filename.isdigit():
+                    file_list.append(int(filename))
+        file_list.sort()
+        return file_list
+
+    async def find_head(m3u8: str) -> Tuple[str, int, List[int]]:
+        change_line = []
+        head_flag = False
+        line_now = 0
+        head = ''
+        https_line = 0
+        with open(m3u8, 'r', encoding='UTF-8', errors='ignore') as f:
+            for line in f:
+                line = line.strip()
+                line_now += 1
+                if not head_flag and "AES-128" in line:
+                    index = line.find('URI=')
+                    head = line[:index + 4]
+                    https_line = line_now
+                    head_flag = True
+                if head_flag and "http" in line:
+                    change_line.append(line_now)
+        return head, https_line, change_line
+
+    async def replace_m3u8(flist: List[str], aes128_line: int, new_head: str, file_sorted: List[int], path: str, ts_end: str, change_line: List[int]) -> str:
+        flist[aes128_line - 1] = f"{new_head}\n"
+        for u, i in enumerate(change_line[1:]):
+            if u < len(file_sorted):
+                flist[i - 1] = f"{path.replace(os.sep, '/')}/{file_sorted[u]}{ts_end}\n"
+        new_m3u8 = os.path.join(path, ".m3u8").replace(os.sep, '/')
+        with open(new_m3u8, 'w', encoding='UTF-8', errors='ignore') as f:
+            f.writelines(flist)
+        return new_m3u8
+
+    async def process_folder(path: str, m3u8: str, key: str, ts_end: str, output_video: str):
+        aes128_prior, aes128_line, change_line = await find_head(m3u8)  # 找出aes128开头内容、所在行和需要修改的行
+        new_head = f"{aes128_prior}\"{key}\""
+        file_sorted = await sort_func(path)   # 文件名排序
+        with open(m3u8, 'r', encoding='UTF-8', errors='ignore') as f:   # 复制一份
+            flist = f.readlines()
+        new_m3u8 = await replace_m3u8(flist, aes128_line, new_head, file_sorted, path, ts_end, change_line)  # 产生基于本地文件的m3u8
+        time.sleep(0.5)
+        await cmd_pool(new_m3u8, output_video, "aes128", semaphore)   # 根据不同模式 创建转换任务
+        if os.path.exists(path + '/.m3u8'):
+            os.remove(path + '/.m3u8')
+
+    # 提取数据 并创建执行异步任务列表
+    async def process_all_folders():
+        tasks = []
+        for i in range(len(encrypted_folders)):
+            path = encrypted_folders[i]
+            video_name = os.path.basename(path)
+            path_q = os.path.dirname(path) + "/"
+            output_video = os.path.join(path_q, f"{video_name}.mp4").replace(os.sep, '/')
+            if os.path.exists(output_video.replace('.m3u8_contents', '')):
+                print(f'存在同名文件 {output_video}')
+                continue
+            m3u8 = key_map[i]
+            key = key_files[i].replace(os.sep, '/')
+
+            task = process_folder(path, m3u8, key, ts_end, output_video)
+            tasks.append(task)
+        await asyncio.gather(*tasks)  # 执行任务列表
+
+    await process_all_folders()
+
+
+async def Normal(unencrypted_folders, ts_end, semaphore):
+    async def sort_func(folder: str) -> List[int]:
+        file_list = []
+        for dirpath, dirnames, filenames in os.walk(folder):
+            for filename in filenames:
+                if len(filename) < 10 and filename.isdigit():
+                    file_list.append(int(filename))
+        file_list.sort()
+        return file_list
+
+    async def process_folder(path: str):
+        file_list = await sort_func(path)
+        tmp_txt = os.path.join(path, '.txt')
+        video_name = os.path.basename(path.rstrip('/'))
+        path_q = os.path.dirname(path) + "/"
+        if os.path.exists(path_q + video_name.replace('.m3u8_contents', '') + '.mp4'):
+            print('存在同名文件', path_q + video_name.replace('.m3u8_contents', '') + '.mp4')
+            return
+
+        with open(tmp_txt, 'w', encoding='UTF-8', errors='ignore') as f:
+            for i in file_list:   # 片段路径写入txt中
+                f.write(f"file '{path}/{i}{ts_end}'\n")
+        output_video = os.path.join(path_q, f"{video_name}.mp4")
+        await cmd_pool(tmp_txt, output_video, "normal", semaphore)  # 根据不同模式 创建转换任务
+
+        if os.path.exists(tmp_txt):
+            os.remove(tmp_txt)
+    await asyncio.gather(*(process_folder(folder) for folder in unencrypted_folders))
+
+
+async def main(media, ts_end, count):
+    semaphore = asyncio.Semaphore(count)
+    if not ts_end:
+        print("---------没有指定ts后缀，默认为空---------")
+        print("--------- 若想指定，请5秒内取消 ---------")
+        time.sleep(5)
+    if media:
+        start_time = time.time()
+        folder = check_media(media)  # 查找给定路径下的文件夹
+        result = tag_folder(folder, ts_end)  # 选出文件名为纯数字且数量大于5个的文件夹
+        encrypted_folders, unencrypted_folders, key_files, key_map = classify_folders(result, "key")
+        if encrypted_folders:  # 有加密视频
+            await AES128(encrypted_folders, key_files, key_map, ts_end, semaphore)
+        if unencrypted_folders:
+            await Normal(unencrypted_folders, ts_end, semaphore)
+        end_time = time.time()
+        print("用时：", end_time - start_time, "秒")
+    else:
+        print("请提供 --media 参数")
+        exit()
+
+
+if __name__ == "__main__":
+    assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
+
+    media = input("请输入媒体文件路径（media）：")
+    ts_end = input("请输入ts文件后缀（默认为空）：")
+    count_input = input("请输入计数值（默认为8）：")
+    media = media.strip('"')
+    try:
+        count = int(count_input) if count_input else 8
+    except ValueError:
+        print("计数值必须是一个整数，使用默认值8")
+        count = 8
+
+    asyncio.run(main(media, ts_end, count))
+
+
+
+
+
+
+
+
